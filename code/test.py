06f4130c850eabf84f31e159d2848dba3e8da575
destingong <61211882+destingong@users.noreply.github.com>
2025-02-28 19:24:10 +1300

    Update test.py

--- code/test.py
+++ code/test.py
@@ -1,10 +1,15 @@
 import os
 filepath = '/home/runner/work/SP/SP/data/test.txt'
-print(filepath)
-print('Script working directory', os.getcwd())
+# print('Script working directory', os.getcwd())
 
 # save news data
 import json
 with open(filepath, 'w') as f:
   f.write('\nhello')
-print('File Created', filename)
+
+# Traverse up the directory tree
+while current_dir != os.path.dirname(current_dir):  # Stop at the root directory
+    print(current_dir)
+    current_dir = os.path.dirname(current_dir)
+  
+print('File Created', filepath)
